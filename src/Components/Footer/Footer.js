243bbac52003162a79ba49b298715732fdfab965
chistyhimel <fc_himel@yahoo.com>
2020-08-26 18:15:30 -0700

    completed

---
+++ src/Components/Footer/Footer.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import './Footer.css'
+const Footer = () => {
+    return (
+        <div className="bg-dark text-center text-light">
+            <h6 className="py-5 ">© 2020 Code School by <span className="text-danger"> Himel </span>. All rights reserved.</h6>
+        </div>
+    );
+};
+
+export default Footer;
---
+++ src/fakeData/CourseData.js
@@ -0,0 +1,109 @@
+
+var UserData = [{
+    "id": 1,
+    "name": "NestJS Zero To Hero – Modern TypeScript Back-End Development",
+    "img": "https://freecoursesite.com/wp-content/uploads/2019/07/2053219_e620_2.jpg",
+    "courseFee": 80,
+    "instructor": "Ariel Weinberger"
+  },
+  {
+    "id": 2,
+    "name": "The Modern Javascript Bootcamp Course",
+    "img": "https://freecoursesite.com/wp-content/uploads/2020/03/2634490_e343_2.jpg",
+    "courseFee": 55,
+    "instructor": "Colt Steele"
+  },
+  {
+    "id": 3,
+    "name": "Flutter & Dart – The Complete Guide",
+    "img": "https://freecoursesite.com/wp-content/uploads/2018/08/1708340_7108_2.jpg",
+    "courseFee": 41,
+    "instructor": "Maximilian Schwarzmüller"
+  },
+  {
+    "id": 4,
+    "name": "The Complete React Native + Hooks Course",
+    "img": "https://freecoursesite.com/wp-content/uploads/2019/10/959700_8bd2_11.jpg",
+    "courseFee": 37,
+    "instructor": "Stephen Grider"
+  },
+  {
+    "id": 5,
+    "name": "Angular – The Complete Guide",
+    "img": "https://freecoursesite.com/wp-content/uploads/2017/12/756150_c033_2-696x392.jpg",
+    "courseFee": 65,
+    "instructor": "Maximilian Schwarzmüller"
+  },
+  {
+    "id": 6,
+    "name": "Website Hacking / Penetration Testing & Bug Bounty Hunting",
+    "img": "https://freecoursesite.com/wp-content/uploads/2018/03/984734_90be_6.jpg",
+    "courseFee": 25,
+    "instructor": " Zaid Sabih"
+  },
+  {
+    "id": 7,
+    "name": "Build A Slack Chat App With React, Redux, And Firebase",
+    "img": "https://freecoursesite.com/wp-content/uploads/2018/11/1806572_730f_2.jpg",
+    "courseFee": 45,
+    "instructor": "Reed Barger"
+  },
+  {
+    "id": 8,
+    "name": "The Ultimate Deep Web Guide: Purchase Anything From Markets",
+    "img": "https://freecoursesite.com/wp-content/uploads/2020/04/1200586_5dea_2-750x405-1.jpg",
+    "courseFee": 65,
+    "instructor": "Dark Web Academy"
+  },
+  {
+    "id": 9,
+    "name": "Complete Machine Learning And Data Science: Zero To Mastery",
+    "img": "https://freecoursesite.com/wp-content/uploads/2020/02/2511476_4e38_2.jpg",
+    "courseFee": 39,
+    "instructor": " Andrei Neagoie"
+  },
+  {
+    "id": 10,
+    "name": "AWS Certified Solutions Architect",
+    "img": "https://freecoursesite.com/wp-content/uploads/2018/02/362328_91f3_10.jpg",
+    "courseFee": 30,
+    "instructor": " Ryan Kroonenburg"
+  },
+  {
+    "id": 11,
+    "name": "The Web Developer Bootcamp",
+    "img": "https://freecoursesite.com/wp-content/uploads/2018/03/625204_436a_2.jpg",
+    "courseFee": 19,
+    "instructor": "Colt Steele"
+  },
+  {
+    "id": 12,
+    "name": "The Complete Guide To Testing Javascript & Node Applications",
+    "img": "https://freecoursesite.com/wp-content/uploads/2020/03/2324088_fb9c_5.jpg",
+    "courseFee": 75,
+    "instructor": "Kati Frantz"
+  },
+  {
+    "id": 13,
+    "name": "Complete Python Developer: Zero To Mastery",
+    "img": "https://freecoursesite.com/wp-content/uploads/2019/12/2473048_8255_4.jpg",
+    "courseFee": 97,
+    "instructor": "Andrei Neagoie"
+  },
+  {
+    "id": 14,
+    "name": "The Complete Flutter UI Masterclass | IOS & Android In Dart",
+    "img": "https://freecoursesite.com/wp-content/uploads/2020/03/2666528_43f1_3.jpg",
+    "courseFee": 66,
+    "instructor": "Marcus Ng"
+  },
+  {
+    "id": 15,
+    "name": "WordPress Development – Themes, Plugins & Gutenberg",
+    "img": "https://freecoursesite.com/wp-content/uploads/2020/02/546415151-768x452.png",
+    "courseFee": 27,
+    "instructor": " Luis Ramirez Jr"
+  },
+];
+
+export default UserData;
