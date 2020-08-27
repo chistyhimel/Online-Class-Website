8edbf14be597734b580afc77eeec2a66a5fd6567
chistyhimel <fc_himel@yahoo.com>
2020-08-26 19:08:18 -0700

    bug fixed

--- src/Components/Course/Course.js
+++ src/Components/Course/Course.js
@@ -16,7 +16,7 @@ const Course = (props) => {
                                 <h5 className="card-title">{name}</h5>
                                 <p className="card-text">With supporting text below as a natural lead-in to additional content.</p>
                                 <h6><FontAwesomeIcon icon={faChalkboardTeacher} /> Instructor : {instructor}</h6>
-                                <h6 className="my-4"><FontAwesomeIcon icon={faTags} />Price : $ {courseFee}</h6>
+                                <h6 className="my-4"><FontAwesomeIcon icon={faTags} />Price : $ {courseFee}.00</h6>
                                 <button className="btn btn-primary" onClick={()=>props.handleAddClick(props.name)}><FontAwesomeIcon icon={faShoppingCart} /> Enroll Now</button>
                             </div>
                     </div>
